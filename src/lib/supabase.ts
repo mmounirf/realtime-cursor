8a4560b11f9a5fdbe53eb7ec13f8183366e87631
Mou <m.mounir.f@gmail.com>
2025-11-26 22:54:31 +0300

    fix: supabase client instance

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
+import { type Database } from "@/database.types";
 import { createClient } from "@supabase/supabase-js";
 
-export function supabase() {
-  return createClient(
-    import.meta.env.VITE_SUPABASE_URL!,
-    import.meta.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!
-  );
-}
+const supabase = createClient<Database>(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!
+);
+
+export default supabase;
