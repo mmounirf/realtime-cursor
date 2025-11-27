4fec966ccc494b4556461acc9239d1fff0322ef4
Mou <m.mounir.f@gmail.com>
2025-11-27 15:53:33 +0300

    feat: add sonner toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Toaster richColors />
   </StrictMode>
 );
