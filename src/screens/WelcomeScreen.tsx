a5edf95d7ebbf02e2d5fb0123cd1ea038b81a0ff
Mou <m.mounir.f@gmail.com>
2025-11-27 14:31:35 +0300

    feat: WelcomeScreen UI

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,10 +1,20 @@
 import { AnimatedCursors } from "@/components/AnimatedCursors";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { DotPattern } from "@/components/ui/dot-pattern";
+import { Input } from "@/components/ui/input";
+import { NeonGradientCard } from "@/components/ui/neon-gradient-card";
 import { cn } from "@/lib/utils";
 
 export default function WelcomeScreen() {
   return (
-    <div>
+    <div className="container max-w-2xl">
       <DotPattern
         width={20}
         height={20}
@@ -12,11 +22,26 @@ export default function WelcomeScreen() {
         cy={1}
         cr={1}
         className={cn(
-          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)]"
+          "mask-[linear-gradient(to_bottom_right,white,transparent,transparent)]"
         )}
-      />{" "}
+      />
       <AnimatedCursors />
-      <h1>Welcome Screen</h1>
+      <NeonGradientCard>
+        <Card className="bg-transparent shadow-none border-none p-0">
+          <CardHeader className="p-0">
+            <CardTitle>Join Room</CardTitle>
+            <CardDescription>
+              The name will be visible to all room joiners
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col p-0">
+            <Input placeholder="Write your name" type="text" />
+          </CardContent>
+          <Button size="lg" variant="outline">
+            Join
+          </Button>
+        </Card>
+      </NeonGradientCard>
     </div>
   );
 }
