c031e835c79c79162b83df07a07d62a99cb91734
Mou <m.mounir.f@gmail.com>
2025-11-28 00:10:03 +0300

    feat: add useRealtimeCursors hook

---
+++ src/hooks/useRealtimeCursors.tsx
@@ -0,0 +1,167 @@
+import { channel } from "@/lib/supabase";
+import {
+  RealtimeChannel,
+  REALTIME_SUBSCRIBE_STATES,
+} from "@supabase/supabase-js";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const useThrottleCallback = <Params extends unknown[], Return>(
+  callback: (...args: Params) => Return,
+  delay: number
+) => {
+  const lastCall = useRef(0);
+  const timeout = useRef<number | null>(null);
+
+  return useCallback(
+    (...args: Params) => {
+      const now = Date.now();
+      const remainingTime = delay - (now - lastCall.current);
+
+      if (remainingTime <= 0) {
+        if (timeout.current) {
+          clearTimeout(timeout.current);
+          timeout.current = null;
+        }
+        lastCall.current = now;
+        callback(...args);
+      } else if (!timeout.current) {
+        timeout.current = setTimeout(() => {
+          lastCall.current = Date.now();
+          timeout.current = null;
+          callback(...args);
+        }, remainingTime);
+      }
+    },
+    [callback, delay]
+  );
+};
+
+const generateRandomColor = () =>
+  `hsl(${Math.floor(Math.random() * 360)}, 100%, 70%)`;
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
+
+const EVENT_NAME = "realtime-cursor-move";
+
+type CursorEventPayload = {
+  position: {
+    x: number;
+    y: number;
+  };
+  user: {
+    id: number;
+    name: string;
+  };
+  color: string;
+  timestamp: number;
+};
+const throttleMs = 50;
+
+export const useRealtimeCursors = ({ username }: { username: string }) => {
+  const [color] = useState(generateRandomColor());
+  const [userId] = useState(generateRandomNumber());
+  const [cursors, setCursors] = useState<Record<string, CursorEventPayload>>(
+    {}
+  );
+  const cursorPayload = useRef<CursorEventPayload | null>(null);
+
+  const channelRef = useRef<RealtimeChannel | null>(null);
+
+  const callback = useCallback(
+    (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+
+      const payload: CursorEventPayload = {
+        position: {
+          x: clientX,
+          y: clientY,
+        },
+        user: {
+          id: userId,
+          name: username,
+        },
+        color: color,
+        timestamp: new Date().getTime(),
+      };
+
+      cursorPayload.current = payload;
+
+      channelRef.current?.send({
+        type: "broadcast",
+        event: EVENT_NAME,
+        payload: payload,
+      });
+    },
+    [color, userId, username]
+  );
+
+  const handleMouseMove = useThrottleCallback(callback, throttleMs);
+
+  useEffect(() => {
+    channel
+      .on("presence", { event: "leave" }, ({ leftPresences }) => {
+        leftPresences.forEach(function (element) {
+          setCursors((prev) => {
+            if (prev[element.key]) {
+              delete prev[element.key];
+            }
+
+            return { ...prev };
+          });
+        });
+      })
+      .on("presence", { event: "join" }, () => {
+        if (!cursorPayload.current) return;
+
+        channelRef.current?.send({
+          type: "broadcast",
+          event: EVENT_NAME,
+          payload: cursorPayload.current,
+        });
+      })
+      .on(
+        "broadcast",
+        { event: EVENT_NAME },
+        (data: { payload: CursorEventPayload }) => {
+          const { user } = data.payload;
+          // Don't render your own cursor
+          if (user.id === userId) return;
+
+          setCursors((prev) => {
+            if (prev[userId]) {
+              delete prev[userId];
+            }
+
+            return {
+              ...prev,
+              [user.id]: data.payload,
+            };
+          });
+        }
+      )
+      .subscribe(async (status) => {
+        if (status === REALTIME_SUBSCRIBE_STATES.SUBSCRIBED) {
+          await channel.track({ key: userId });
+          channelRef.current = channel;
+        } else {
+          setCursors({});
+          channelRef.current = null;
+        }
+      });
+
+    return () => {
+      channel.unsubscribe();
+      channelRef.current = null;
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [handleMouseMove]);
+
+  return { cursors };
+};
