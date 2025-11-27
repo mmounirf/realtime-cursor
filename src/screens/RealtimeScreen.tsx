60950b0ed1bd1ef397e9cb0bb1a27656830db7b0
Mou <m.mounir.f@gmail.com>
2025-11-28 00:10:25 +0300

    feat: consume realtime cursors and render cursors in realtime screen

--- src/screens/RealtimeScreen.tsx
+++ src/screens/RealtimeScreen.tsx
@@ -1,12 +1,21 @@
+import Cursor from "@/components/Cursor";
 import { Button } from "@/components/ui/button";
+import { useRealtimeCursors } from "@/hooks/useRealtimeCursors";
 import { useAppStore } from "@/store/app";
 
 export default function RealtimeScreen() {
-  const { signOut } = useAppStore();
+  const { signOut, auth } = useAppStore();
+
+  const { cursors } = useRealtimeCursors({
+    username: auth?.user.user_metadata.display_name,
+  });
+
   return (
     <div>
-      <h1>Realtime Screen</h1>
       <Button onClick={() => signOut()}>Sign out</Button>
+      {Object.keys(cursors).map((id) => (
+        <Cursor point={[cursors[id].position.x, cursors[id].position.y]} />
+      ))}
     </div>
   );
 }
