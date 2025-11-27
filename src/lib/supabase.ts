8f468bf689e2f60a28f8608a09f08eb75cd59f36
Mou <m.mounir.f@gmail.com>
2025-11-27 13:04:36 +0300

    fix: supabase env varibles

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { type Database } from "@/database.types";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_OR_ANON_KEY!
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 export default supabase;
