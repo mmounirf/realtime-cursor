3ce90e929843220dbadab7f04d084e74a6d20f16
Mou <m.mounir.f@gmail.com>
2025-11-27 13:40:01 +0300

    feat: WelcomeScreen animated background

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,6 +1,21 @@
+import { AnimatedCursors } from "@/components/AnimatedCursors";
+import { DotPattern } from "@/components/ui/dot-pattern";
+import { cn } from "@/lib/utils";
+
 export default function WelcomeScreen() {
   return (
     <div>
+      <DotPattern
+        width={20}
+        height={20}
+        cx={1}
+        cy={1}
+        cr={1}
+        className={cn(
+          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)]"
+        )}
+      />{" "}
+      <AnimatedCursors />
       <h1>Welcome Screen</h1>
     </div>
   );
